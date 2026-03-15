8ed8b4417e60c011a68937e029048298811699f6
Jabberwockyjib <bdoud@develotype.com>
2026-03-15 14:40:16 -0400

    Restore Omarchy installer helpers

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -3,3 +3,20 @@
 set -eEo pipefail
 
 source "$FOUNDATIONOS_INSTALL/helpers/log.sh"
+
+helper_root="${OMARCHY_INSTALL:-$FOUNDATIONOS_INSTALL}/helpers"
+
+source_helper_if_present() {
+  local helper_name="$1"
+  local helper_path="$helper_root/$helper_name"
+
+  if [[ -f $helper_path ]]; then
+    # shellcheck disable=SC1090
+    source "$helper_path"
+  fi
+}
+
+source_helper_if_present chroot.sh
+source_helper_if_present presentation.sh
+source_helper_if_present errors.sh
+source_helper_if_present logging.sh
--- install/helpers/log.sh
+++ install/helpers/log.sh
@@ -2,30 +2,42 @@
 
 set -eEo pipefail
 
-foundationos_step() {
-  echo "[foundationos] $*"
-}
+if ! declare -F foundationos_step >/dev/null 2>&1; then
+  foundationos_step() {
+    echo "[foundationos] $*"
+  }
+fi
 
-foundationos_info() {
-  echo "[foundationos][info] $*"
-}
+if ! declare -F foundationos_info >/dev/null 2>&1; then
+  foundationos_info() {
+    echo "[foundationos][info] $*"
+  }
+fi
 
-foundationos_warn() {
-  echo "[foundationos][warn] $*" >&2
-}
+if ! declare -F foundationos_warn >/dev/null 2>&1; then
+  foundationos_warn() {
+    echo "[foundationos][warn] $*" >&2
+  }
+fi
 
-foundationos_error() {
-  echo "[foundationos][error] $*" >&2
-}
+if ! declare -F foundationos_error >/dev/null 2>&1; then
+  foundationos_error() {
+    echo "[foundationos][error] $*" >&2
+  }
+fi
 
-foundationos_die() {
-  foundationos_error "$*"
-  exit 1
-}
+if ! declare -F foundationos_die >/dev/null 2>&1; then
+  foundationos_die() {
+    foundationos_error "$*"
+    exit 1
+  }
+fi
 
-run_logged() {
-  local script="$1"
+if ! declare -F run_logged >/dev/null 2>&1; then
+  run_logged() {
+    local script="$1"
 
-  foundationos_step "Running $(basename "$script")"
-  "$script"
-}
+    foundationos_step "Running $(basename "$script")"
+    "$script"
+  }
+fi
